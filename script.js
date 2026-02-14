71b079db3528b23701bda9b215d1630b016f6315
Anastasia <tsutskova_a.v@mail.ru>
2026-02-15 00:29:19 +0300

    my first project

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
-window.onload = function(){
-    new Slider({
-        images: '.gallery .photo img',
-        next: '.gallery .tabs .next',
-        prev: '.gallery .tabs .prev',
-        interval: '2000'
-    })
-    // ES5
-    function Slider(obj) {
-        this.images = document.querySelectorAll(obj.images)
-        this.btNext = document.querySelector(obj.next)
-        this.btPrev = document.querySelector(obj.prev)
+const slide = document.getElementById("slide");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const reaction = document.getElementById("reaction");
 
-        this.prev = function() {
-            console.log('prev');
+const images = [
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+  "images/5.jpg",
+  "images/6.jpg",
+  "images/7.jpg",
+  "images/8.jpg",
+];
 
-        }
+let current = 0;
+let lastClickTime = 0;
 
-         this.next = function() {
-            console.log('next');
-                }
-         this.btNext.addEventListner('click', this.next)
-         this.btPrev.addEventListner('click', this.prev)
+function showImage() {
+  slide.src = images[current];
+}
 
+nextBtn.addEventListener("click", () => {
+  current = (current + 1) % images.length;
+  showImage();
+  checkFastClick();
+});
+
+prevBtn.addEventListener("click", () => {
+  current = (current - 1 + images.length) % images.length;
+  showImage();
+  checkFastClick();
+});
+
+function checkFastClick(){
+    const now = Date.now();
+    const diff = now - lastClickTime;
+
+    if(lastClickTime !== 0 && diff < 300){
+        showReaction();
     }
+    lastClickTime = now;
+}
 
-    //ES6
+function showReaction(){
+    const messages = [
+        "ohhhhhh...",
+        "Не останавливайся",
+        "Ещё...",
+        "Дааа...",
+        "Быстрее... мммм..."
+    ];
 
+const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+reaction.textContent = randomMessage;
+
+const randomX = Math.random() * 60 + 20;
+const randomY = Math.random() * 60 +20;
+
+reaction.style.left = randomX + "%";
+reaction.style.top = randomY + "%";
+
+reaction.classList.add("show");
+
+setTimeout(() => {
+    reaction.classList.remove("show");
+}, 1500);
 }
