6b1966f50c3a2922c103e23e492278565f13a9a7
Anastasia <tsutskova_a.v@mail.ru>
2026-02-14 21:54:14 +0300

    my first project

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-const image = document.getElementById("img1");
-image.addEvetListner("click", function (){
-    image.src = "images/2.jpg";
-})
+window.onload = function(){
+    new Slider({
+        images: '.gallery .photo img',
+        next: '.gallery .tabs .next',
+        prev: '.gallery .tabs .prev',
+        interval: '2000'
+    })
+    // ES5
+    function Slider(obj) {
+        this.images = document.querySelectorAll(obj.images)
+        this.btNext = document.querySelector(obj.next)
+        this.btPrev = document.querySelector(obj.prev)
+
+        this.prev = function() {
+            console.log('prev');
+
+        }
+
+         this.next = function() {
+            console.log('next');
+                }
+         this.btNext.addEventListner('click', this.next)
+         this.btPrev.addEventListner('click', this.prev)
+
+    }
+
+    //ES6
+
+}
