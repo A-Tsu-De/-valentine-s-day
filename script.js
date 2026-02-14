6ecad6a337c3184b7162ff82674da68861d2ae07
Anastasia <tsutskova_a.v@mail.ru>
2026-02-14 21:38:00 +0300

    my first project

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const images = ["1.jpg", "2.jpg", "3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"]; // Массив фото
-let currentIndex = 0; // Индекс текущей картинки
-const imgElement = document.getElementById("image");
-
-// Функция переключения
-function changeImage(direction) {
-    currentIndex += direction;
-
-    // Зацикливание: если конец, то в начало
-    if (currentIndex >= images.length) currentIndex = 0;
-    if (currentIndex < 0) currentIndex = images.length - 1;
-
-    imgElement.src = images[currentIndex]; // Смена src [1]
-}
-
-// Обработчики событий
-document.getElementById("prev").addEventListener("click", () => changeImage(-1));
-document.getElementById("next").addEventListener("click", () => changeImage(1));
+const image = document.getElementById("img1");
+image.addEvetListner("click", function (){
+    image.src = "images/2.jpg";
+})
